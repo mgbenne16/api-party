b363cc497f953981c9de66fd9ae5b0afe6ba66e0
Megan Bennett <mgbenne16@earlham.edu>
2018-06-18 14:19:25 -0400

    Add header content and style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='App-header'>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </div>
       </div>
-    );
+    )
   }
 }
 
